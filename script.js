4d9e52273ab08c711ad4184026ddaa7411d9243c
Nícolas <161242131+nicz-gom@users.noreply.github.com>
2025-01-07 23:06:39 -0300

    versão 2.0

--- script.js
+++ script.js
@@ -3,13 +3,18 @@
 //  1 - A lista de produtos deve conter os seguintes produtos: Computador, Telefone, Mouse, Teclado Exiba essa lista no console quando abrir o index.html. (OK)
 //  2 - Mostre no console quantos produtos tem nessa lista. (OK)
 //  3 - Retire o produto Mouse da lista e retorne no console a lista com os produtos restantes. (OK)
-//  4 - Faça uma busca na sua lista por algum produto, por exemplo procure por Computador e caso exista esse produto na sua lista exiba um console falando que este produto existe e mostre tambem o nome deste produto caso não exista mostre uma mensagem avisão que o produto não foi encontrado.
+//  4 - Faça uma busca na sua lista por algum produto, por exemplo procure por Computador e caso exista esse produto na sua lista exiba um console falando que este produto existe e mostre tambem o nome deste produto caso não exista mostre uma mensagem avisando que o produto não foi encontrado.
 //  5 - Remova o segundo item da sua lista.
 
 let lista = ['Computador', 'Telefone', 'Mouse', 'Teclado'];
 let quantidade = lista.length;
 
+localStorage.setItem("@lista", JSON.stringify(lista));
+
 console.log(`1 - Os elementos do lista são: ${lista}`);
+
+console.log('==============================');
+
 console.log(`2 - A quantidade de elementos na lista é: ${quantidade}`);
 
 lista.splice(2, 1); 
@@ -21,13 +26,27 @@ lista.splice(2, 1);
     Os números a diante - são responsáveis por adicionar
 */
 
+console.log('==============================');
 
 console.log(`3 - Os elementos do lista, após a remoção, são: ${lista}`);
 
-lista.find((item) =>{
-    let itemDaLista = item;
-    return 
-})
-    
+console.log('==============================');
 
-localStorage.setItem("@lista", JSON.stringify(lista));
+let resultado = lista.find((item)=>{
+    return item == 'Telefone'; 
+});
+
+if(resultado != undefined){
+    console.log(`4 - ${resultado} existe na lista`);
+}else{
+    console.log(`4 - O produto não foi encontrado`);
+}
+
+console.log('==============================');
+
+let result = Number(resultado);
+
+lista.splice(result, 1);
+
+console.log(result);
+
