faddc16362bce7b73833010a5ce64727d75163f7
Nícolas <161242131+nicz-gom@users.noreply.github.com>
2025-01-09 22:54:34 -0300

    3.0

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 //  4 - Faça uma busca na sua lista por algum produto, por exemplo procure por Computador e caso exista esse produto na sua lista exiba um console falando que este produto existe e mostre tambem o nome deste produto caso não exista mostre uma mensagem avisando que o produto não foi encontrado.
 //  5 - Remova o segundo item da sua lista.
 
+console.log('\n Primeiro Exercício! \n\n');
+
 let lista = ['Computador', 'Telefone', 'Mouse', 'Teclado'];
 let quantidade = lista.length;
 
@@ -38,15 +40,32 @@ let resultado = lista.find((item)=>{
 
 if(resultado != undefined){
     console.log(`4 - ${resultado} existe na lista`);
+
+    console.log('==============================');
+
+    let j = 0;
+    
+    for(i = 0; i != lista.length; i++){
+       
+        if(lista[i] == resultado)
+        {
+            for(i; i != lista.length; i++){
+                lista[i] = lista[i + 1];
+            }
+            console.log(`5 - Os elementos da lista, após a remoção, são: ${lista}`);
+            break;
+        }
+    }
+
 }else{
     console.log(`4 - O produto não foi encontrado`);
 }
 
-console.log('==============================');
+console.log('\n Segundo Exercício! \n\n');
 
-let result = Number(resultado);
 
-lista.splice(result, 1);
+// Crie uma lista de apenas numeros 1,3,5,7,0,9​
 
-console.log(result);
-
+//  Ordene essa lista do menor para o maior.
+//  Retire o primeiro numero desta lista.
+//  Inverta toda ordem da sua lista por exemplo: [1,3,5,7,9] para [9,7,5,3,1]
